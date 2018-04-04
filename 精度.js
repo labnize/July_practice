e39c160f1e218e5b4d2334d1555288a82a60dbe2
labnize <july.zhuxh@foxmail.com>
2018-04-04 13:41:11 +0800

    精度问题

---
+++ 精度.js
@@ -0,0 +1,13 @@
+var a=0.29 * 100; console.log(a)
+28.999999999999996
+
+var a = 0.29 * 100; a = Math.round(a * 100) / 100; console.log(a);
+29
+
+var a = 0.2999 * 100; a = Math.round(a * 100) / 100; console.log(a);
+29.99
+
+var a = 0.29022239 * 100; a = Math.round(a * 100) / 100; console.log(a);
+29.02
+
+// 当整数时，显示整数；当小数时显示小数点后两位。
